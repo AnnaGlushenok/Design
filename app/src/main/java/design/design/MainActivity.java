8c5469a7b9497f1757212db6b6147da4086aa067
annas <annasan14052003@gmail.com>
2023-08-28 18:21:33 +0300

    completed

--- app/src/main/java/design/design/MainActivity.java
+++ app/src/main/java/design/design/MainActivity.java
@@ -2,7 +2,18 @@ package design.design;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Insets;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.view.WindowMetrics;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View layoutView = findViewById(R.id.layout);
+        ViewTreeObserver layoutObserver = layoutView.getViewTreeObserver();
+        layoutObserver.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                layoutView.getViewTreeObserver().removeOnPreDrawListener(this);
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int height = displayMetrics.heightPixels;
+                System.out.println(height);
+                System.out.println(layoutView.getHeight());
+                if (height > layoutView.getHeight())
+                    findViewById(R.id.content_button).setVisibility(View.INVISIBLE);
+                else
+                    findViewById(R.id.button).setVisibility(View.INVISIBLE);
+
+                return false;
+            }
+        });
     }
 }
