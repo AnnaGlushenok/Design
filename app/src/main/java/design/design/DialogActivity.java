14908d3d7b2e86b87a618b774de5fa57204e3871
annas <annasan14052003@gmail.com>
2023-09-26 12:37:11 +0300

    completed 2 task

--- app/src/main/java/design/design/DialogActivity.java
+++ app/src/main/java/design/design/DialogActivity.java
@@ -1,10 +1,10 @@
 package design.design;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DialogActivity extends AppCompatActivity {
 
--- app/src/main/java/design/design/MainActivity.java
+++ app/src/main/java/design/design/MainActivity.java
@@ -1,81 +1,97 @@
 package design.design;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.Objects;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private CompoundView compoundView;
     private TextView text;
     private SharedPreferences sharedPreferences;
-    private long timeAppWasMinimized;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         compoundView = findViewById(R.id.compound_view);
         compoundView.setRadius(getIntent().getIntExtra("msg", 0) + 10);
 
         text = (TextView) findViewById(R.id.label);
         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        timeAppWasMinimized = sharedPreferences.getLong("timeAppWasMinimized", 0);
+
+        updateFontSizePreference(- 1);
+        sharedPreferences.edit()
+                .putLong(Saves.TIME.name(), 0)
+                .apply();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        float savedTextSize = sharedPreferences.getFloat("textSize", 0);
-        if (savedTextSize != 0) {
-            text.setTextSize(TypedValue.COMPLEX_UNIT_PX, savedTextSize);
+        float newTextSize = sharedPreferences.getFloat(Saves.TEXT_SIZE.name(), 0);
+        if (newTextSize == - 1) {
+            updateFontSizePreference(text.getTextSize());
+            return;
         }
+        updateFontSizePreference(newTextSize + 2);
+        text.setTextSize(TypedValue.COMPLEX_UNIT_PX, newTextSize + 2);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        long currentTime = System.currentTimeMillis();
-        long timeDifference = (currentTime - timeAppWasMinimized) / (1000 * 60);
-        float textSize = text.getTextSize() + 5 - (2 * timeDifference);
-        text.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        float textSize = sharedPreferences.getFloat(Saves.TEXT_SIZE.name(), 0);
+        long time = sharedPreferences.getLong(Saves.TIME.name(), 0);
+        if (time == 0)
+            return;
+
+        float newTextSize = Math.abs(textSize - (float) (System.currentTimeMillis() - time) / 30000);
+        text.setTextSize(newTextSize);
+        updateFontSizePreference(newTextSize);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        timeAppWasMinimized = System.currentTimeMillis();
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putLong("timeAppWasMinimized", timeAppWasMinimized);
-        editor.apply();
+        float newTextSize = sharedPreferences.getFloat(Saves.TEXT_SIZE.name(), 0) + 5;
+        updateFontSizePreference(newTextSize);
+
+        text.setTextSize(TypedValue.COMPLEX_UNIT_PX, newTextSize);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putFloat("textSize", text.getTextSize());
-        editor.apply();
+        sharedPreferences.edit()
+                .putLong(Saves.TIME.name(), System.currentTimeMillis())
+                .apply();
     }
 
     @Override
     protected void onDestroy() {
+        updateFontSizePreference(text.getTextSize());
         super.onDestroy();
-        System.out.println("destroy");
+    }
+
+    private void updateFontSizePreference(float newSize) {
+        sharedPreferences.edit()
+                .putFloat(Saves.TEXT_SIZE.name(), newSize)
+                .apply();
     }
 
     public void open(View view) {
         startActivity(new Intent(this, DialogActivity.class).putExtra("msg", compoundView.getRadius()));
     }
+}
+
+enum Saves {
+    TEXT_SIZE, TIME
 }
